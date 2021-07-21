366a6068ccf7dbd2848d6280ec8db2ce3ba8a7ad
newname <newemail>
2021-07-21 14:38:46 +0800

    second commit

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ from catkin_pkg.python_setup import generate_distutils_setup
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
     version='0.0.0',
-    scripts=['node/boxes_fusion_node.py'],
-    packages=['boxes_fusion'], 
+    scripts=['node/traffic_count_node.py'],
+    packages=['traffic_count'], 
     package_dir={'': 'src'},
     requires=['std_msgs', 'rospy', 'sensor_msgs']
 )
